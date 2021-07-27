3e524138d16d99e7cf8f4d351b765ce93219b0ab
Sereda Vladyslav <v.u.sereda@gmail.com>
2021-07-27 21:14:04 +0300

    complete task

---
+++ src/js/index.js
@@ -0,0 +1,39 @@
+console.log('------------- #3')
+function sum (a) {
+    let result = a
+    return function (b) {
+        return result += b
+    }
+}
+
+let sum1 = sum(1)
+console.log(sum1(2))
+console.log(sum(1)(2))
+
+console.log('------------- #4')
+/** Первый вариант
+ *
+ * При испольвании var выделяется одна ячейка памяти,
+ * при использовании let выделяется ячейка под каждую итерацию, ES6)
+ */
+/*for (let i = 0; i < 10; i++) {
+    setTimeout(function() {
+        console.log(i);
+    }, 0);
+}*/
+
+/** Второй вариант
+ *
+ * Ошибка из-за того что лексическое окружение создается один раз,
+ * i будет присвоено последнее значение из цикла
+ */
+for (var i = 0; i < 10; i++) {
+    setTimeout(print(i))
+}
+
+function print(i) {
+   let count = i
+   return function () {
+        console.log(count)
+   }
+}
